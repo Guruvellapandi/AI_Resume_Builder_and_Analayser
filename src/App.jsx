2a8b0cd1abdd3e49d77072fde6da78817f47ba2c
Vishalganapathy123 <143880586+Vishalganapathy123@users.noreply.github.com>
2025-03-18 14:41:46 +0530

    Resume Page added

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import TemplateSelection from "./pages/TemplateSelection";
 import ResumeEditor from "./pages/ResumeEditor";
+import ResumePage from "./pages/ResumePage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<ResumePage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/templates" element={<TemplateSelection />} />
         <Route path="/resume-editor" element={<ResumeEditor />} />
